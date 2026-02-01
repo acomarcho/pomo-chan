314108e277d333562731725b7aaa5b3fefeecdaf
acomarcho <marchotridyo@heynovo.ai>
2026-02-01 20:34:17 +0700

    Use CJS eslint config for absolute aliases

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+const path = require("node:path");
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/electron",
+    "plugin:import/typescript",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project: [path.join(__dirname, "tsconfig.json")],
+      },
+      alias: {
+        map: [
+          ["@/lib", path.join(__dirname, "src/lib")],
+          ["@/components", path.join(__dirname, "src/components")],
+          ["@", path.join(__dirname, "src")],
+        ],
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+      },
+    },
+  },
+};
