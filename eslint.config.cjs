5a7f3275cd2afec2d798e9f70fee27dd20ad08cd
acomarcho <marchotridyo@heynovo.ai>
2026-02-03 17:13:12 +0700

    chore: migrate to eslint 9 flat config

---
+++ eslint.config.cjs
@@ -0,0 +1,45 @@
+const path = require("node:path");
+const globals = require("globals");
+const js = require("@eslint/js");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/electron",
+    "plugin:import/typescript"
+  ),
+  {
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: [path.join(__dirname, "tsconfig.json")],
+        },
+        alias: {
+          map: [["@", path.join(__dirname, "src")]],
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      },
+    },
+  },
+];
